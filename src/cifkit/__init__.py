6ac9b0b4fcbf008096b5c01ad1f6e3bd3907bb23
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-09-21 16:11:53 -0400

    Add version to init

--- src/cifkit/__init__.py
+++ src/cifkit/__init__.py
@@ -1,5 +1,8 @@
+from importlib.metadata import version
+
 from .data.example import Example
 from .models.cif import Cif
 from .models.cif_ensemble import CifEnsemble
 
+__version__ = version("cifkit")
 __all__ = ["Cif", "CifEnsemble", "Example"]
